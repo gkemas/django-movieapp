bfbc5c067de445f0cf5ee96174c8a82e01a74707
gokhan <kemasgokhan@gmail.com>
2022-08-19 18:37:02 +0300

    ff

--- movies/views.py
+++ movies/views.py
@@ -6,27 +6,27 @@ film_liste=[
     {
         "film_adı":"film 1",
         "aciklama":"film_1 açıklama",
-        "resim":"https://picsum.photos/200/300?random=1",
+        "resim":"1.jpeg",
         "anasayfa":True
     },
     {
         "film_adı":"film 2",
         "aciklama":"film_2 açıklama",
-        "resim":"https://picsum.photos/200/300?random=2",
+        "resim":"2.jpeg",
         "anasayfa":True
 
     },
     {
         "film_adı":"film 3",
         "aciklama":"film_3 açıklama",
-        "resim":"https://picsum.photos/200/300?random=3",
+        "resim":"3.jpeg",
         "anasayfa":False
 
     },
     {
         "film_adı":"film 4",
         "aciklama":"film_4 açıklama",
-        "resim":"https://picsum.photos/200/300?random=4",
+        "resim":"4.jpeg",
         "anasayfa":False
 
     }
